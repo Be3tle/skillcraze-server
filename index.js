6cb65cedd243ff7ec7df64002ef6683c6a671f53
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-19 00:11:45 +0600

    cors bug fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173', ''],
+    origin: ['http://localhost:5173', 'https://skill-craze.web.app'],
     credentials: true,
   })
 );
